7c6ec43563b551f0769f43f1585324e61e22dd6a
ramshankartv <tvramshankar@gmail.com>
2024-04-20 11:45:50 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>react pizza</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza
+        alt="pizza"
+        src="pizza/mango.png"
+        pizzaName="Pizza Spinachi"
+        pizzaDes="Tomato, mozarella, spnich, and ricotta cheese"
+        price={10}
+      ></Pizza>
+      <Pizza
+        alt="pizza"
+        src="pizza/order.png"
+        pizzaName="Pizza Spegeti"
+        pizzaDes="Potatto, spnich, cheese, and butter"
+        price={12}
+      ></Pizza>
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img alt={props.alt} src={props.src}></img>
+      <div>
+        <h1>{props.pizzaName}</h1>
+        <p>{props.pizzaDes}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  // const hour = new Date().getHours();
+  // const openHour = 12;
+  // const closeHour = 22;
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We're currently open");
+  // } else {
+  //   alert("Sorry we're closed");
+  // }
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open
+    </footer>
   );
 }
 
